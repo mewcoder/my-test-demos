aa89301ded83162fac92b0b1a074632743c72158
mewcoder <1010953107@qq.com>
2022-07-31 19:45:06 +0800

    chore: add todo

---
+++ vue-unit-test/src/views/Todo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Todo />
+  </div>
+</template>
+<script>
+import Todo from '@/components/Todo';
+
+export default {
+  name: 'TodoApp',
+  components: {
+    Todo
+  }
+};
+</script>
